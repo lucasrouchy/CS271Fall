065c3867b4019a1673b9cbb69f1244086cba3d90
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2021-11-23 15:13:30 -0800

    Exercise 1,2,3

---
+++ cploration/c09/parser.h
@@ -0,0 +1,65 @@
+//exercise 2
+#ifndef __PARSER_H__
+#define __PARSER_H__
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "hack.h"
+
+
+
+// All code in file
+
+
+
+#define MAX_LINE_LENGTH 200
+#define MAX_LABEL_LENGTH MAX_LINE_LENGTH -2
+#define MAX_HACK_ADDRESS INT16_MAX
+#define MAX_INSTRUCTIONS MAX_HACK_ADDRESS
+
+
+
+
+char* strip(char* s);
+void parse(FILE* file);
+bool is_Atype(const char *);
+bool is_label(const char *);
+bool is_Ctype(const char *);
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+char *extract_label(const char *line, char* label);
+enum inst_type {
+  invalid = -1,
+  Atype,
+  Ctype,
+};
+typedef struct
+c_instruction {
+  opcode a: 1;
+  opcode comp: 6;
+  opcode dest:3;
+  opcode jump: 3;
+
+
+}c_instruction;
+
+typedef struct
+a_instruction {
+  union hack_addr {
+      hack_addr address;
+      char * label;
+  }hack_addr;
+  bool is_addr;
+}a_instruction;
+
+typedef struct
+instruction {
+  union AorC {
+    enum inst_type Atype;
+    enum inst_type Ctype;
+  }AorC;
+}instruction;
+#endif
---
+++ cploration/c09/symtable.c
@@ -0,0 +1,55 @@
+#include "symtable.h"
+
+// Exercice 3: Writing the hash function
+int hash(char* str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash % SYMBOL_TABLE_SIZE;
+}
+//Exercice 4
+void symtable_insert(char* key, hack_addr addr) {
+    Symbol* item = (Symbol*) malloc(sizeof(Symbol));
+    item->address = addr;
+    item->name = key;
+
+    int hashIndex = hash(key);
+
+
+    while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->name != NULL) {
+
+        ++hashIndex;
+        hashIndex %= SYMBOL_TABLE_SIZE;
+   }
+
+   hashArray[hashIndex] = item;
+}
+//Exercice 5
+Symbol* symtable_find(char* key) {
+    int hashIndex = hash(key);
+
+    while(hashArray[hashIndex] != NULL) {
+      if(hashArray[hashIndex]->name == key)
+          return hashArray[hashIndex];
+
+      ++hashIndex;
+
+      hashIndex %= SYMBOL_TABLE_SIZE;
+    }
+
+    return NULL;
+}
+
+//Exercice 6
+void symtable_display_table(void) {
+    for(int i = 0; i < SYMBOL_TABLE_SIZE; i++) {
+        if(hashArray[i] != NULL)
+            printf(" (%s,%d)", hashArray[i]->name, hashArray[i]->address);
+        else
+            printf(" ~~ ");
+    }
+   printf("\n");
+}
